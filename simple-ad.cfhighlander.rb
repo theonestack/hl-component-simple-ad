40412ed18c961c642a82a93dcdb6907fe82ca508
Samseppiol <j_hage@live.com>
2021-03-05 13:04:53 +1100

    init

---
+++ simple-ad.cfhighlander.rb
@@ -0,0 +1,18 @@
+CfhighlanderTemplate do
+  Name 'simple-ad'
+  Description "simple-ad - #{component_version}"
+
+  Parameters do
+    ComponentParam 'EnvironmentName', 'dev', isGlobal: true
+    ComponentParam 'EnvironmentType', 'development', allowedValues: ['development','production'], isGlobal: true
+    ComponentParam 'Size', 'Small', allowedValues: ['Small','Large']
+    ComponentParam 'VPCId', type: 'AWS::EC2::VPC::Id'
+    ComponentParam 'SubnetIds', type: 'CommaDelimitedList'
+    ComponentParam 'DnsDomain'
+    ComponentParam 'SSMParameterPath', '/simple-ad/${EnvironmentName}/master_password'
+    ComponentParam 'Identifier', component_name
+  end
+
+  Component name: 'securepassword', template: 'github:theonestack/hl-component-password-generator#master.snapshot', render: Inline
+
+end
---
+++ simple-ad.cfndsl.rb
@@ -0,0 +1,20 @@
+CloudFormation do
+
+  name = external_parameters.fetch(:name, "${EnvironmentName}.${DnsDomain}")
+  sso = external_parameters.fetch(:sso, nil)
+  short_name = external_parameters.fetch(:short_name, nil)
+
+  DirectoryService_SimpleAD(:Directory) {
+    Description FnSub("Simple AD for #{name}")
+    EnableSso sso unless sso.nil?
+    Name FnSub(name)
+    Password "Fn::GetAtt" => ['PasswordSSMSecureParameter', 'Password']
+    Size Ref(:Size)
+    ShortName short_name unless short_name.nil?
+    VpcSettings ({
+      SubnetIds: Ref(:SubnetIds),
+      VpcId: Ref(:VPCId)
+    })
+  }
+
+end
